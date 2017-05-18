720d684f06893933ffdcedb74dc3f2765a9ff64b
Ionut Cojocaru <adn.ionut@gmail.com>
2017-05-18 09:33:44 +0300

    Admin initial commit

---
+++ TownHall/TownHall/Models/ApplicationRoleListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TownHall.Models
+{
+    public class ApplicationRoleListViewModel
+    {
+        public string Id { get; set; }
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+        public int NumberOfUsers { get; set; }
+    }
+
+}
